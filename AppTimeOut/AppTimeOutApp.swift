c12ad6a80a9502710e2b40c5bb0d438640dadb1e
Harry Huynh <harryhuynh08@gmail.com>
2025-11-06 07:14:18 -0600

    Initial Commit

---
+++ AppTimeOut/AppTimeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTimeOutApp.swift
+//  AppTimeOut
+//
+//  Created by Harry Hoa Huynh on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AppTimeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTimeOut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTimeOut
+//
+//  Created by Harry Hoa Huynh on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
